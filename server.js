e8953128f09b876f6d3b94e932d2490218d08bd6
mariedevoll <marie.devoll@gmail.com>
2024-04-27 18:39:48 -0400

    working on the server.js

--- server.js
+++ server.js
@@ -1,4 +1,127 @@
-const express = require('express');
-const { Pool } = require('pg');
-const PORT = process.env.PORT || 3001;
-const app = express();
+
+const inquirer = require('inquirer');
+
+//prompt user for choices
+const promptUser = () => {
+    inquirer.prompt([
+        {
+        name: 'choices',
+        type: 'list',
+        message: 'Please choose one of the following options:',
+        choices: [
+            'View all Employees', //presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+            'View all Departments', //presented with a formatted table showing department names and department ids
+            'View all Roles', //presented with the job title, role id, the department that role belongs to, and the salary for that role
+            'Add a Department', //prompted to enter the name of the department and that department is added to the database
+            'Add a Role', //prompted to enter the name, salary, and department for the role and that role is added to the database
+            'Add an Employee', //prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+            'Update an Employee Role', //prompted to select an employee to update and their new role and this information is updated in the database
+            'View All Employees by Department',
+            'Remove an Employee',
+            'Update an Employee Manager',
+            'Remove Role',
+            'View Department Budgets',
+            'Remove Department'
+        ]
+    }
+])
+.then((answers) => {
+    const {choices} = answers;
+
+        if (choices === 'View all Employees') {
+        viewAllEmployees();
+         }   
+        if (choices === 'View all Departments') {
+            viewAllDepartments();
+        }
+        if (choices === 'View all Roles') {
+            viewAllRoles();
+        } 
+        if (choices === 'Add a Department') {
+            addDepartment();
+        }
+        if (choices === 'Add a Role') {
+            addRole();
+        }
+        if (choices === 'Add an Employee') {
+            addEmployee();
+        }
+        if (choices ===  'Update an Employee Role') {
+            updateEmployeeRole();
+        }
+        if (choices === 'View All Employees by Department') {
+            viewEmployeesByDepartment();
+        }
+        if (choices === 'Remove an Employee') {
+            removeEmployee();
+        }
+        if (choices === 'Update an Employee Manager') {
+            udpateEmployeeManager();
+        }
+        if (choices === 'Remove Role') {
+            removeRole();
+        }
+        if (choices === 'View Department Budgets') {
+            viewDepartmentBudget();
+        }
+        if (choices === 'Remove Department') {
+            removeDepartment();
+        }
+    });
+};
+
+
+//view all employees
+const viewAllEmployees = () => {
+    let sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, 
+            department.department_name AS 'department', role.salary FROM employee, role, department 
+            WHERE department.id = role.department_id AND role.id = employee.role_id 
+            ORDER BY employee.id ASC`;
+    connection.promise().query(sql, (error, response) => {
+        if (error) throw error;
+        promptUser();
+    });
+};
+
+//view all departments
+const viewAllDepartments = () => {
+    const sql = `SELECT department.id AS id, department.department_name 
+        AS department FROM department`;
+    connection.promise().query(sql, (error, response) => {
+        if (error) throw error;
+        promptUser();
+    });
+};
+
+//view all roles
+const viewAllRoles = () => {
+    const sql = `SELECT role.id, role.title, department.department_name AS department 
+            FROM role INNER JOIN department ON role department_id = department.id`;
+    connection.promise().query(sql, (error, response) => {
+        if (error) throw error;
+        response.forEach((role) => {console.log(role.title);
+        });
+        promptUser();
+    });
+};
+
+//add a department
+const addDepartment = () => {
+    inquirer
+    .prompt([
+        {
+            name: 'newDepartment',
+            type: 'input',
+            message: 'What is the name of the new department?',
+            validate: validate.validateString
+        }
+    ])
+    .then((answer) => {
+        let sql = `INSERT INTO department (department_name VALUES (?)`;
+        connection.query(sql, answer.newDepartment, (error, response) => {
+            if (error) throw error;
+            viewAllDepartments();
+        });
+    });
+};
+
